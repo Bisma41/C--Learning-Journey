174617a141da1922531639349a79d50185f28512
BISMA SAJJAD <152860495+Bisma41@users.noreply.github.com>
2025-06-18 00:38:31 -0700

    Create ArrayLoopExample.cs

---
+++ Day-01/ArrayLoopExample.cs
@@ -0,0 +1,15 @@
+namespace Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+        }
+    }
+
+}
