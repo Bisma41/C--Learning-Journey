82f700b4f616df126e38ed3d95d153b0ab39dfa2
BISMA SAJJAD <152860495+Bisma41@users.noreply.github.com>
2025-06-22 00:23:48 -0700

    Create Program.cs

---
+++ Day 02/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Name = "Bisma";
+            p1.Age = 21;
+            p1.Introduce();
+        }
+    }
+}
